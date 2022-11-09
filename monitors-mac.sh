eda501cfcf6f995fbf177132ba8537a174ffc2d4
danielxhogan <danielxhogan@gmail.com>
2022-11-09 15:07:24 -0800

    create seperate i3 config and monitors.sh for mac

---
+++ monitors-mac.sh
@@ -0,0 +1,3 @@
+# xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --output DP-1 --primary --mode 1680x1050 --pos 1920x0 --output DP-3 --mode 1920x1080 --pos 3600x0
+feh --bg-scale ~/Pictures/nope-raining-blood.jpeg
+picom --experimental-backends
