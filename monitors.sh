128f2394435d7fb34ca9191b5037decd5f5a0565
danielxhogan <danielxhogan@gmail.com>
2023-05-25 08:34:05 -0700

    add suh, rearrange monitors

--- monitors.sh
+++ monitors.sh
@@ -1,3 +1,3 @@
-xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --output DP-1 --primary --mode 1680x1050 --pos 1920x0 --output DP-3 --mode 1920x1080 --pos 3600x0
+xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --output HDMI-1 --primary --mode 1680x1050 --pos 1920x0 --output DP-3 --mode 1920x1080 --pos 3600x0
 feh --bg-scale ~/Pictures/Wallpapers/steve-lund-castle-color-final-web.jpg
 picom --experimental-backends
