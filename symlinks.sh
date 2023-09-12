6c41adddb16d2609e430c787c575e7a60170ce10
danielxhogan <danielxhogan@gmail.com>
2023-09-11 20:23:48 -0700

    record sym link commands for config files

---
+++ symlinks.sh
@@ -0,0 +1,17 @@
+mkdir ~/.config/i3
+ln -s ~/dotfiles/i3 ~/.config/i3/config
+
+mkdir ~/.config/i3status
+ln -s ~/dotfiles/i3status ~/.config/i3status/config
+
+mkdir ~/.config/alacritty
+ln -s ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+ln -s ~/dotfiles/vscode.json ~/.config/Code/User/settings.json
+
+ln -s ~/dotfiles/qmk/qmk.ini ~/.config/qmk/qmk.ini
+ln -s ~/dotfiles/qmk/kyrisuh ~/qmk_firmware/keyboards/splitkb/kyria/keymaps/kyrisuh
