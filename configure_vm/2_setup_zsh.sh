c2ee98d0f2b49503f5e8be1352ebf71c5abc37d6
danielxhogan <danielxhogan@gmail.com>
2024-11-06 10:47:15 -0800

    fix zsh issues

--- configure_vm/2_setup_zsh.sh
+++ configure_vm/2_setup_zsh.sh
@@ -1,5 +1,5 @@
 # git clone https://github.com/danielxhogan/dotfiles.git
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-zsh
+rm ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
