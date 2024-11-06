f2338266db4dc605daed5b15b724d278e31ed07e
danielxhogan <danielxhogan@gmail.com>
2024-11-05 23:46:26 -0800

    fix cant find oh-my-zsh error

--- configure_vm/2_setup_zsh.sh
+++ configure_vm/2_setup_zsh.sh
@@ -1,5 +1,5 @@
 # git clone https://github.com/danielxhogan/dotfiles.git
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh
