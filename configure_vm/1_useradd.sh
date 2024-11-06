8d4cc053df6e250c3eef84ac018e2b8f74d1abae
danielxhogan <danielxhogan@gmail.com>
2024-11-06 10:46:44 -0800

    add exa and change message

--- configure_vm/1_useradd.sh
+++ configure_vm/1_useradd.sh
@@ -1,10 +1,10 @@
 # git clone https://github.com/danielxhogan/dotfiles.git
 
-apt install vim zsh curl
+apt install vim zsh curl exa
 
 useradd -m hugexjackedman
 passwd hugexjackedman
 usermod -aG sudo hugexjackedman
 
-echo "now you can open alacritty and run:"
-echo "ssh 192.168.1.10"
+echo "now you can open alacritty and ssh in:"
+ip a | grep 192.168.1
