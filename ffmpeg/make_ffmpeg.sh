95331abe385e0b81555465ee64294e007babbaa7
danielxhogan <danielxhogan@gmail.com>
2024-11-08 23:00:58 -0800

    update ffmpeg install scripts

--- ffmpeg/make_ffmpeg.sh
+++ ffmpeg/make_ffmpeg.sh
@@ -6,15 +6,16 @@ echo "\n\n \
   **************** \n" && \
 \
 mkdir -p ~/ffmpeg/ffmpeg_sources ~/ffmpeg/bin && \
+ll ~/ffmpeg && echo && \
 \
-$HOME/dotfiles/make_ffmpeg/1_install_deps.sh && \
-$HOME/dotfiles/make_ffmpeg/2_make_x264.sh && \
-$HOME/dotfiles/make_ffmpeg/3_make_x265.sh && \
-$HOME/dotfiles/make_ffmpeg/4_make_libvpx.sh && \
-$HOME/dotfiles/make_ffmpeg/5_make_libaom.sh && \
-$HOME/dotfiles/make_ffmpeg/6_make_libsvt_av1.sh && \
-$HOME/dotfiles/make_ffmpeg/7_make_libdav1d.sh && \
-$HOME/dotfiles/make_ffmpeg/8_make_libvmaf.sh && \
+$HOME/dotfiles/ffmpeg/install_deps.sh && \
+$HOME/dotfiles/ffmpeg/make_x264.sh && \
+$HOME/dotfiles/ffmpeg/make_x265.sh && \
+$HOME/dotfiles/ffmpeg/make_libvpx.sh && \
+$HOME/dotfiles/ffmpeg/make_libaom.sh && \
+$HOME/dotfiles/ffmpeg/make_libsvt_av1.sh && \
+$HOME/dotfiles/ffmpeg/make_libdav1d.sh && \
+$HOME/dotfiles/ffmpeg/make_libvmaf.sh && \
 \
 echo "\n\n \
   *************** \n \
@@ -52,3 +53,5 @@ PATH="$HOME/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg/ffmpeg_build/lib/pkg
   --enable-libvmaf && \
 PATH="$HOME/ffmpeg/bin:$PATH" make -j$(nproc) && \
 make install
+
+# --enable-libv4l2 \
