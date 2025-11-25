35d198347cca2965f09ece774aa3db56f312e76c
danielxhogan <danielxhogan@gmail.com>
2025-11-24 21:12:19 -0800

    turn off shared libs

--- ffmpeg/make_libsvt_av1.sh
+++ ffmpeg/make_libsvt_av1.sh
@@ -10,6 +10,6 @@ cd ~/ffmpeg/ffmpeg_sources && \
 git -C SVT-AV1 pull 2> /dev/null || git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git && \
 mkdir -p SVT-AV1/build && \
 cd SVT-AV1/build && \
-PATH="$HOME/ffmpeg/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=ON .. && \
+PATH="$HOME/ffmpeg/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg/ffmpeg_build" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
 PATH="$HOME/ffmpeg/bin:$PATH" make -j$(nproc) && \
 make install
