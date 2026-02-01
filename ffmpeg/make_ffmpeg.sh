0a84b4e3564e2805f9c19f6b9f72ded5984e3c70
danielxhogan <danielxhogan@gmail.com>
2026-01-31 17:20:38 -0800

    build ffmpeg with support for vulkan and libplacebo

--- ffmpeg/make_ffmpeg.sh
+++ ffmpeg/make_ffmpeg.sh
@@ -10,33 +10,33 @@ cd ~/ffmpeg/ffmpeg_sources && \
 wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
 tar xjvf ffmpeg-snapshot.tar.bz2 && \
 cd ffmpeg && \
-PATH="$HOME/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg/ffmpeg_build/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig" ./configure \
+PATH="$HOME/ffmpeg/bin:$PATH" ./configure \
   --prefix="$HOME/ffmpeg/ffmpeg_build" \
   --pkg-config-flags="--static" \
-  --extra-cflags="-I$HOME/ffmpeg/ffmpeg_build/include" \
-  --extra-ldflags="-L$HOME/ffmpeg/ffmpeg_build/lib" \
+  --extra-cflags="-I$HOME/ffmpeg/ffmpeg_build/include -I$VULKAN_SDK/include" \
+  --extra-ldflags="-L$HOME/ffmpeg/ffmpeg_build/lib -L$VULKAN_SDK/lib" \
   --extra-libs="-lpthread -lm" \
   --ld="g++" \
   --bindir="$HOME/ffmpeg/bin" \
   --enable-gpl \
-  --enable-gnutls \
-  --enable-libaom \
-  --enable-libass \
-  --enable-libfdk-aac \
-  --enable-libfreetype \
-  --enable-libmp3lame \
-  --enable-libopus \
-  --enable-libsvtav1 \
-  --enable-libdav1d \
-  --enable-libvorbis \
-  --enable-libvpx \
-  --enable-libx264 \
-  --enable-libx265 \
   --enable-nonfree \
   --enable-shared \
+  --enable-gnutls \
+  --enable-libmp3lame \
+  --enable-libfdk-aac \
+  --enable-libvorbis \
+  --enable-libopus \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-libvpx \
+  --enable-libaom \
+  --enable-libsvtav1 \
+  --enable-libdav1d \
+  --enable-libass \
+  --enable-libfreetype \
   --enable-libvmaf \
+  --enable-vulkan \
+  --enable-libplacebo \
   --enable-libzimg && \
 PATH="$HOME/ffmpeg/bin:$PATH" make -j$(nproc) && \
 make install
-
-# --enable-libv4l2 \
