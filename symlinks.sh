9f52362c8859dbc6456a2b7489fe1283ecfeacc0
danielxhogan <danielxhogan@gmail.com>
2026-03-07 17:43:02 -0800

    reorder by priority

--- symlinks.sh
+++ symlinks.sh
@@ -1,25 +1,25 @@
-mkdir ~/.config/i3
-ln -s ~/dotfiles/i3 ~/.config/i3/config
-
-mkdir ~/.config/i3status
-ln -s ~/dotfiles/i3status ~/.config/i3status/config
+rm ~/.zshrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 
 mkdir ~/.config/alacritty
 ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-rm ~/.zshrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-ln -s ~/dotfiles/picom.conf ~/.config/picom.conf
+ln -s ~/dotfiles/mpv/mpv.conf ~/.config/mpv/mpv.conf
+ln -s ~/dotfiles/mpv/input.conf ~/.config/mpv/input.conf
+mkdir ~/.config/mpv/script-opts
+ln -s ~/dotfiles/mpv/script-opts/osc.conf ~/.config/mpv/script-opts/osc.conf
 
 ln -s ~/dotfiles/vscode.json ~/.config/Code/User/settings.json
 
 ln -s ~/dotfiles/qmk/qmk.ini ~/.config/qmk/qmk.ini
 ln -s ~/dotfiles/qmk/kyrisuh ~/qmk_firmware/keyboards/splitkb/kyria/keymaps/kyrisuh
 
-ln -s ~/dotfiles/mpv/mpv.conf ~/.config/mpv/mpv.conf
-ln -s ~/dotfiles/mpv/input.conf ~/.config/mpv/input.conf
-mkdir ~/.config/mpv/script-opts
-ln -s ~/dotfiles/mpv/script-opts/osc.conf ~/.config/mpv/script-opts/osc.conf
+mkdir ~/.config/i3
+ln -s ~/dotfiles/i3 ~/.config/i3/config
+
+mkdir ~/.config/i3status
+ln -s ~/dotfiles/i3status ~/.config/i3status/config
+
+ln -s ~/dotfiles/picom.conf ~/.config/picom.conf
